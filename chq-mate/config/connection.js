dacfe18f126c82a3f1fb5e41b38023942dcfcfb3
mfomin93 <mfomin93@gmail.com>
2020-06-22 20:10:39 -0400

    sql database connection

---
+++ chq-mate/config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "admin",
+    password: "abcd1234",
+    database: "chq_register"
+  });
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;

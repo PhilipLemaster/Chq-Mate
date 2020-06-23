036b61102808229d0883927b1a9cd06fe8950675
Philip Lemaster <philiplemaster@yahoo.com>
2020-06-22 20:19:21 -0400

    jumbotron base component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavbarMain from './components/Navbar/index';
-import EditProfileForm from './components/EditProfileForm/index'
+import EditProfileForm from './components/EditProfileForm/index';
+import JumboSplash from './components/JumboSplash';
 
 function App() {
 
   return (
     <div>
-      <EditProfileForm />
+      {/* <EditProfileForm /> */}
+      <JumboSplash />
     </div>
   );
 }

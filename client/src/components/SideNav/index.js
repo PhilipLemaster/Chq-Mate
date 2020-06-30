e75d2a3f230674095910995ba06f83e9c21e78de
Philip Lemaster <philiplemaster@yahoo.com>
2020-06-30 00:08:34 -0400

    icon updates

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './style.css';
+import homeIcon from '../../assets/icons/home.png';
+import matchIcon from '../../assets/icons/match.png';
+import editIcon from '../../assets/icons/edit.png';
+import topIcon from '../../assets/icons/topgames.png';
+import chatIcon from '../../assets/icons/chat.png';
 
 function SideNav() {
     return(
             <ul id="menu">
-                <a href="#t1"><button className="icon fas fa-home" id="1"></button></a>
-                <a href="#t2"><button className="icon far fa-keyboard" id="2"></button></a>
-                <a href="#t3"><button className="icon fa fa-rocket" id="3"></button></a>
-                <a href="#t4"><button className="icon fas fa-search" id="4"></button></a>
-                <a href="#t5"><button className="icon fas fa-list-ol" id="5"></button></a>
+                <a href="#t1"><img id="1" className="icon far" src={homeIcon} alt='Home Icon'></img></a>
+                <a href="#t2"><img id="2" className="icon far" src={matchIcon} alt='Match Icon'></img></a>
+                <a href="#t3"><img id="3" className="icon far" src={chatIcon} alt='Chat Icon'></img></a>
+                <a href="#t4"><img id="4" className="icon far" src={topIcon} alt='Top Games Icon'></img></a>
+                <a href="#t5"><img id="5" className="icon far" src={editIcon} alt='Edit Icon'></img></a>
             </ul>
     )
 }

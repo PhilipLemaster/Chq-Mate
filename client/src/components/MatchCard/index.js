40c5f8b8f939d5aec4c17d739e77554ecb356287
Philip Lemaster <philiplemaster@yahoo.com>
2020-07-08 22:53:49 -0400

    match card styling

--- client/src/components/MatchCard/index.js
+++ client/src/components/MatchCard/index.js
@@ -3,18 +3,23 @@ import { Card, Button, CardTitle, CardImg, CardText } from 'reactstrap';
 import { useAuth0 } from "@auth0/auth0-react";
 import './style.css';
 import competitiveIcon from '../../assets/icons/competitive.png';
+import pcIcon from '../../assets/icons/pc.png';
 
 function MatchCard() {
 
     const { user } = useAuth0();
 
     return(
-        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} className="matchCard">
+        <Card body inverse style={{ backgroundColor: '#232136' }} className="matchCard">
             <img src={competitiveIcon} alt="typeIcon" className="typeIcon"></img>
-            <CardImg src={user.picture} className="matchImg"/>
-            <CardTitle className="cardGTag">Special Title Treatment</CardTitle>
-            <CardText className="">With supporting text below as a natural lead-in to additional content.</CardText>
-            <Button>Button</Button>
+            <img src={pcIcon} alt="consoleIcon" className="consoleIcon"></img>
+            <div>
+                <CardImg src={user.picture} className="matchImg"/>
+                <h2 className="cardGTag">ZeusOrangejuice</h2>
+                <p className="cardFGame">Super Smash Bros.</p>
+                <p className="cardBio">I'm just a cool d00d from Charlotte. I main Dr. Mario in Smash and play jungle on LoL. Hit me up yo.</p>
+                <Button className="cardBut">Message</Button>
+            </div>
         </Card>
     )
 }
